90801bcaa728c031bc768070203de0eb6b6d245c
collins-mwangi1 <collins.mwangi1@student.moringaschool.com>
2024-03-28 17:21:07 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(){}
+  function scuberGreetingForFeet(feet){
+if (feet <= 400) {
+  return 'this one is on me!';
+} else if (feet <= 2000) {
+  return 'that will be twenty bucks.';
+}else if (2000 < feet && feet <= 2500) {
+  return 'i will glardly take your thirty bucks.';
+} else {
+  return 'none can do';
+}
 }
 
+  
+
+
 function ternaryCheckCity(){
-  // Write your code here!
+  function ternaryCheckCity(city) {
+    return city === 'NYC' ? 'OK, sounds good.'
+  }
 }
 
 function switchOnCharmFromTip(){
-  // Write your code here!
+  function switchOnCharmFromTip(tip) {
+    switch (tip) {
+      case 'generous':
+        return 'thank you so much.';
+        case 'not as generous':
+          return 'thank you.';
+          default:
+            return 'bye.';
+    }
+  }
 }
